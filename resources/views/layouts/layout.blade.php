43772d31569246b764c6f660b085a86ba6bf9c46
Trenton Craig <tcraig@remindermedia.net>
2017-09-09 23:03:11 -0400

    Incorporated npm/bootstrap

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,1 +1,16 @@
-<?php
+<html>
+    <head>
+        <title>Cafe Nokios - @yield('title')</title>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <div class="title">
+                <h1>Cafe Nokios</h1>
+            </div>
+        </div>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
